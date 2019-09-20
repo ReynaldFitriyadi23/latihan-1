5f5a06a32c2c0bd1589755a7f53cef3586c05cef
ReynaldFitriyadi23 <55323710+ReynaldFitriyadi23@users.noreply.github.com>
2019-09-20 23:04:28 +0700

    Add files via upload

---
+++ latihan-1.php
@@ -0,0 +1,27 @@
+<?php
+// Luas Segitiga
+$alas_segitiga=15;
+$tinggi_segitiga=4;
+$luas_segitiga=1/2*$alas_segitiga*$tinggi_segitiga;
+echo "<h1>1. Luas Segitiga<br>";
+echo "Alas = $alas_segitiga<br>";
+echo "Tinggi = $tinggi_segitiga<br>";
+echo"Luas = $luas_segitiga<br><br>";
+
+// Luas Lingkaran
+$jari_jari=35;
+$phi=22/7;
+$luas_lingkaran=$phi*$jari_jari*$jari_jari;
+echo "<h1>2. Luas Lingkaran <br>";
+echo "Jari - Jari = $jari_jari<br>";
+echo "Luas = $luas_lingkaran<br><br>";
+
+// Luas Jajargenjang
+$alas_jajargenjang=25;
+$tinggi_jajargenjang=14;
+$luas_jajargenjang=$alas_jajargenjang*$tinggi_jajargenjang;
+echo "<h1>3. Luas Jajargenjang<br>";
+echo "Alas = $alas_jajargenjang<br>";
+echo "Tinggi = $tinggi_jajargenjang<br>";
+echo "Luas = $luas_jajargenjang";
+?>
